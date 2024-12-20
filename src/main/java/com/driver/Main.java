9520e7a99cb5220f6bd3f17721415f152ca4db7c
Parvez202295 <parvezhassan1998@gmail.com>
2024-12-20 19:47:12 +0530

    second time change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -16,6 +16,8 @@ public class Main {
     public static void main(String[] args) {
         B b = new B();
         System.out.print(b.meth());
+        B obj = new B();
+        System.out.println(obj.meth());
 
 
 
