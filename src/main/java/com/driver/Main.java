3c96164f59ecc63e461bc7af4dbd0b9f40563e0b
Parvez202295 <parvezhassan1998@gmail.com>
2024-12-20 19:23:34 +0530

    meth added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
+class A{
+    public  static String  meth (){
+        return  "Invoking method from class A";
+    }
+
+}
+class B extends A{
+
+public  static String  meth (){
+    return "Method is overridden in Extendend class B";
+}
+}
 
 public class Main {
+    public static void main(String[] args) {
+        B b = new B();
+        System.out.print(b.meth());
+
+
+
+
+    }
   
 }
